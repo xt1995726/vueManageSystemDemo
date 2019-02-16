7177df747c848bb5c38f936d23a75c66beefc325
xt1995726 <503634677@qq.com>
2019-02-16 21:29:30 +0800

    header

---
+++ src/components/common/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="header">
+    <!-- 折叠按钮 -->
+    <div class="collapse-btn">
+      <i class="el-icon-menu"></i>
+    </div>
+    <div class="logo">后台管理系统</div>
+    <div class="header-right">
+      <div class="header-user-con">
+        <!-- 全屏显示 -->
+        <div class="btn-fullscrenn">
+          <el-tooltip effect="dark" :content="fullscreen?`取消全屏`:`全屏`" placement="bottom">
+            <i class="el-icon-rank"></i>
+          </el-tooltip>
+        </div>
+        <!-- 消息中心 -->
+        <div class="btn-bell">
+          <el-tooltip effect="dark" :content="message?`有${message}条未读消息`:`消息中心`" palcement="bottom">
+            <i class="el-icon-bell"></i>
+          </el-tooltip>
+          <span class="btn-bell-badge" v-if="message"></span>
+        </div>
+        <!-- 用户头像 -->
+
+        <!-- 用户名下拉菜单 -->
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      collapse: false,
+      fullscreen: false,
+      name: 'xietian',
+      message: 2
+
+    }
+  }
+}
+</script>
+<style>
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css"; // 默认主题
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI, {
+  size: "small"
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
