1a778683aa4b3828340f3f48ce49800c4082278a
xt1995726 <503634677@qq.com>
2019-02-17 20:31:37 +0800

    sidebar

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sidebar">
+    <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#324157" text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
+
+    </el-menu>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+</style>
+
