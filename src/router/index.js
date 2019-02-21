7004fd495a714a9da9e89d40a1a365d6f03996bb
xt1995726 <503634677@qq.com>
2019-02-21 21:36:56 +0800

    sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Header from '@/components/common/Header'
+import Sidebar from '@/components/common/Sidebar'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Header',
-      component: Header
+      name: 'Sidebar',
+      component: Sidebar
     }
   ]
 })
