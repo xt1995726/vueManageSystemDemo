531f0afb4679b1f1b2ea2b9061d36086b9404997
xt1995726 <503634677@qq.com>
2019-02-24 21:28:11 +0800

    Baseform

--- src/components/page/BaseForm.vue
+++ src/components/page/BaseForm.vue
@@ -1,11 +1,60 @@
 <template>
   <div>
-    BaseForm
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 表单</el-breadcrumb-item>
+        <el-breadcrumb-item>基本表单</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="container">
+      <div class="form-box">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="表格名称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="选择器">
+            <el-select v-model="form.region" placeholder="请选择">
+              <el-option key="bbk" label="步步高" value="bbk"></el-option>
+              <el-option key="xtc" label="小天才" value="xtc"></el-option>
+              <el-option key="imoo" label="imoo" value="imoo"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="日期时间">
+            <el-col :span="11">
+              <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            </el-col>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
+
 </template>
 <script>
 export default {
+  name: 'baseform',
+  data() {
+    return {
+      options: [
 
+      ],
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: true,
+        type: ['步步高'],
+        resource: '小天才',
+        desc: '',
+        options: []
+      }
+    }
+  }
 }
 </script>
 <style>
