3c8464b37644ea3ad35101b8a4807ab3afdd531a
xt1995726 <503634677@qq.com>
2019-02-27 21:22:11 +0800

    add Tabs

---
+++ src/components/page/Tabs.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="">
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item><i class="el-icon-lx-copy"></i> tab选项卡</el-breadcrumb-item>
+      </el-breadcrumb>
+      <div class="container">
+        <el-tabs v-model="message">
+          <el-tab-pane :label="`未读消息(${unread.length})`" name="first">
+
+          </el-tab-pane>
+          <el-tab-pane :label="`已读消息(${read.length})`" name="second">
+
+          </el-tab-pane>
+          <el-tab-pane :label="`回收站(${recycle.length})`" name="third">
+
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'tabs',
+  data() {
+    return {
+      message: 'first',
+      showHeader: false,
+      unread: [{
+        date: '2018-04-19 20:00:00',
+        title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护',
+      }, {
+        date: '2018-04-19 21:00:00',
+        title: '今晚12点整发大红包，先到先得',
+      }],
+      read: [{
+        date: '2018-04-19 20:00:00',
+        title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
+      }],
+      recycle: [{
+        date: '2018-04-19 20:00:00',
+        title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
+      }]
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/common/Home";
 import Form from "@/components/page/BaseForm";
+import Tabs from "@/components/page/Tabs";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
           path: "/form",
           name: "Form",
           component: Form
+        },
+        {
+          path: "/tabs",
+          name: "Tabs",
+          component: Tabs
         }
       ]
     }
