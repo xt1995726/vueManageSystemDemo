ce99d100bedcc008e253d28f2cdaa9a285eec201
xt1995726 <503634677@qq.com>
2019-03-09 21:08:56 +0800

    table finished

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/common/Home";
-import Form from "@/components/page/BaseForm";
+import BaseForm from "@/components/page/BaseForm";
 import Tabs from "@/components/page/Tabs";
+import BaseTable from "@/components/page/BaseTable";
 
 Vue.use(Router);
 
@@ -15,13 +16,18 @@ export default new Router({
       children: [
         {
           path: "/form",
-          name: "Form",
-          component: Form
+          name: "BaseForm",
+          component: BaseForm
         },
         {
           path: "/tabs",
           name: "Tabs",
           component: Tabs
+        },
+        {
+          path: "/table",
+          name: "BaseTable",
+          component: BaseTable
         }
       ]
     }
