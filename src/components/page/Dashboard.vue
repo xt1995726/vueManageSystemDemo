c6e9a9f4d7d7d549c3f5ac407bba05a2f97e65cd
xt1995726 <503634677@qq.com>
2019-03-12 21:08:30 +0800

    userinfo card

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -1,18 +1,70 @@
 <template>
   <div>
     <el-row :gutter="20">
-			<el-col>
-				<el-card>
-
-				</el-card>
-			</el-col>
-		</el-row>
+      <el-col :span="8">
+        <el-card shadow="hover" class="mgb20" style="height:252px;">
+          <div class="user-info">
+            <img src="../../assets/img/img.jpg" class="user-avator" alt="">
+            <div class="user-info-cont">
+              <div class="user-info-name">{{name}}</div>
+              <div>{{role}}</div>
+            </div>
+          </div>
+          <div class="user-info-list">上次登录时间：<span>2020-01-01</span></div>
+          <div class="user-info-list">上次登录地点：<span>上海</span></div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
 export default {
-
+  name: 'dashboard',
+  data() {
+    return {
+      name: localStorage.getItem('ms_username'),
+    }
+  },
+  computed: {
+    role() {
+      localStorage.setItem('ms_username', 'xietian');
+      return this.name === 'xietian' ? '超级管理员' : '普通用户';
+    }
+  }
 }
 </script>
-<style>
+<style scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+.user-info {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: 2px solid #ccc;
+  margin-bottom: 20px;
+}
+.user-avator {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+}
+.user-info-cont {
+  padding-left: 50px;
+  color: #999;
+  font-size: 14px;
+  flex: 1;
+}
+.user-info-name {
+  font-size: 30px;
+  color: #222;
+}
+.user-info-list {
+  color: #999;
+  font-size: 14px;
+  line-height: 25px;
+}
+.user-info-list span {
+  margin-left: 70px;
+}
 </style>
