c3e1d9f8c86586872891ee60aa9dbff81e905874
xt1995726 <503634677@qq.com>
2019-03-20 20:40:47 +0800

    draglist finish

--- src/components/page/DragList.vue
+++ src/components/page/DragList.vue
@@ -2,7 +2,7 @@
   <section class="main">
     <div class="crumbs">
       <el-breadcrumb separator="/">
-        <el-breadcrum-item><i class="el-icon-rank"></i> 拖拽组件</el-breadcrum-item>
+        <el-breadcrumb-item><i class="el-icon-rank"></i> 拖拽组件</el-breadcrumb-item>
         <el-breadcrumb-item>拖拽排序</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
@@ -13,6 +13,26 @@
           <draggable v-model="todo" @remove="removeHandle" :options="dragOptions">
             <transition-group tag='div' id='todo' class="item-ul">
               <div v-for="item in todo" class="drag-list" :key="item.id">
+                {{item.content}}
+              </div>
+            </transition-group>
+          </draggable>
+        </div>
+        <div class="drag-box-item">
+          <div class="item-title">doing</div>
+          <draggable v-model="doing" @remove='removeHandle' :options="dragOptions">
+            <transition-group tag="div" id="doing" class="item-ul">
+              <div v-for="item in doing" :key="item.id" class="drag-list">
+                {{item.content}}
+              </div>
+            </transition-group>
+          </draggable>
+        </div>
+        <div class="drag-box-item">
+          <div class="item-title">done</div>
+          <draggable v-model="done" @remove='removeHandle' :options="dragOptions">
+            <transition-group tag="div" id="done" class="item-ul">
+              <div v-for="item in done" :key="item.id" class="drag-list">
                 {{item.content}}
               </div>
             </transition-group>
@@ -90,4 +110,59 @@ export default {
 }
 </script>
 <style>
+.drag-box {
+  display: flex;
+  user-select: none;
+}
+.drag-box-item {
+  flex: 1;
+  max-width: 330px;
+  min-width: 300px;
+  height: 600px;
+  margin-right: 16px;
+  background-color: #eff1f5;
+  border: 1px #e1e4e8 solid;
+  border-radius: 6px;
+}
+.item-title {
+  padding: 8px 8px 8px 12px;
+  font-weight: 600;
+  font-size: 14px;
+  color: #24292e;
+  line-height: 1.5;
+}
+.drag-list {
+  padding: 10px;
+  border-radius: 6px;
+  background-color: #fff;
+  margin: 5px 0 10px;
+  border: 1px #e1e4e8 solid;
+  list-style: none;
+  cursor: pointer;
+  transition: border 0.3s ease-in;
+  -webkit-transition: border 0.3s ease-in;
+}
+.item-ul {
+  padding: 0 8px 8px;
+  height: 500px;
+  overflow: scroll;
+}
+.item-ul::-webkit-scrollbar {
+  width: 0;
+}
+.drag-list:hover {
+  border: 1px solid #20a0ff;
+}
+.drag-title {
+  font-weight: 400;
+  line-height: 25px;
+  margin: 10px 0;
+  font-size: 22px;
+  color: #1f2f3d;
+}
+.ghost-style {
+  display: block;
+  color: transparent;
+  border-style: dashed;
+}
 </style>
